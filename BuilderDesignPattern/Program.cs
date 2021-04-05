7b6db417b2022fe0ad894fafa63c723ff2c53f5d
Abdul_Wahab <awanabdulwahab7@gmail.com>
2021-04-05 19:44:15 +0500

    Builder Design Pattern is implemented

---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicleCreator = new VehicleCreator(new HeroBuilder());
+            vehicleCreator.CreateVehicle();
+            var vehicle = vehicleCreator.GetVehicle();
+            vehicle.ShowInfo();
+
+            Console.WriteLine("---------------------------------------------");
+
+            vehicleCreator = new VehicleCreator(new HondaBuilder());
+            vehicleCreator.CreateVehicle();
+            vehicle = vehicleCreator.GetVehicle();
+            vehicle.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
